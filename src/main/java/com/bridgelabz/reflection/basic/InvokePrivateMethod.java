a05dc2859afb04ea4f9ae85e0d7466c6450a0e6b
Akki <chauhanakshit61@gmail.com>
2025-04-07 20:45:37 +0530

    [Akshit] ADD: Invoking private method

---
+++ src/main/java/com/bridgelabz/reflection/basic/InvokePrivateMethod.java
@@ -0,0 +1,34 @@
+package com.bridgelabz.reflection.basic;
+
+import java.lang.reflect.Method;
+
+class Calculator {
+    private int multiply(int a, int b) {
+        return a * b;
+    }
+}
+
+public class InvokePrivateMethod {
+    public static void main(String[] args) {
+        try {
+            Calculator calc = new Calculator();
+
+            // Get the Class object
+            Class<?> cls = calc.getClass();
+
+            // Get the private method 'multiply' with two int parameters
+            Method method = cls.getDeclaredMethod("multiply", int.class, int.class);
+
+            // Make the method accessible
+            method.setAccessible(true);
+
+            // Invoke the method with arguments 6 and 7
+            int result = (int) method.invoke(calc, 6, 7);
+
+            System.out.println("Result of multiply(6, 7): " + result);
+
+        } catch (Exception e) {
+            e.printStackTrace();
+        }
+    }
+}
